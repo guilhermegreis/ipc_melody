63f6147c8e30b926421d9d326f3af8e495cfb39f
Guilherme Reis <guiglreis@gmail.com>
2024-09-11 00:05:13 -0300

    complete: server-client communication setup by integrating server and client

--- clientes.c
+++ clientes.c
@@ -1,50 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
 #include <string.h>
-#include <time.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define BUFFER_SIZE 100
+#define PIPE_NUM "/tmp/pipe_num"
+#define PIPE_STR "/tmp/pipe_str"
 
-int pipe_num[2];
-int pipe_str[2];
-
-void *client_request_numbers(void *arg)
-{
+void client() {
     char buffer[BUFFER_SIZE];
-    while (1)
-    {
+
+    while (1) {
         printf("Requesting Number...\n");
-        read(pipe_num[0], buffer, BUFFER_SIZE);
-        printf("Received Number: %s\n", buffer);
-        sleep(2);
-    }
-}
+        int fd_num = open(PIPE_NUM, O_RDONLY);
+        read(fd_num, buffer, BUFFER_SIZE); 
+        printf("Número recebido: %s\n", buffer);
+        close(fd_num);
 
-void *client_request_strings(void *arg)
-{
-    char buffer[BUFFER_SIZE];
-    while (1)
-    {
         printf("Requesting String...\n");
-        read(pipe_str[0], buffer, BUFFER_SIZE);
-        printf("Received String: %s\n", buffer);
-        sleep(2);
+        int fd_str = open(PIPE_STR, O_RDONLY);
+        read(fd_str, buffer, BUFFER_SIZE);
+        printf("String recebida: %s\n", buffer);
+        close(fd_str);
+
+        sleep(1);
     }
 }
 
-int main()
-{
-    pipe(pipe_num);
-    pipe(pipe_str);
-
-    pthread_t client_number, client_string;
-    pthread_create(&client_number, NULL, client_request_numbers, NULL);
-    pthread_create(&client_string, NULL, client_request_strings, NULL);
-
-    pthread_join(client_number, NULL);
-    pthread_join(client_string, NULL);
-
+int main() {
+    client();
     return 0;
 }
--- servidor.c
+++ servidor.c
@@ -2,88 +2,126 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <string.h>
 #include <time.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define BUFFER_SIZE 100
+#define THREAD_POOL_SIZE 4 
+#define MAX_TASKS 10     
+#define PIPE_NUM "/tmp/pipe_num"
+#define PIPE_STR "/tmp/pipe_str"
 
-int pipe_num[2];
-int pipe_str[2];
-int request_counter = 0;
+int request_counter = 0;  
 
-pthread_mutex_t lock;
+pthread_mutex_t lock;   
+pthread_cond_t cond;   
 
-void *respond_with_number(void *arg)
-{
-    char buffer[BUFFER_SIZE];
-    while (1)
-    {
-        int number = rand() % 100 + 1;
-        snprintf(buffer, BUFFER_SIZE, "%d", number);
-        write(pipe_num[1], buffer, strlen(buffer) + 1);
+typedef struct {
+    void (*function)(void* arg);      
+    void* arg;                    
+} Task;
 
-        pthread_mutex_lock(&lock);
-        request_counter++;
-        printf("Número enviado: %d (Contagem de requisições: %d)\n", number, request_counter);
-        pthread_mutex_unlock(&lock);
-        sleep(1);
+Task task_queue[MAX_TASKS];  
+int task_count = 0;            
+
+void add_task(void (*function)(void* arg), void* arg) {
+    pthread_mutex_lock(&lock);
+    if (task_count < MAX_TASKS) {
+        task_queue[task_count].function = function;
+        task_queue[task_count].arg = arg;
+        task_count++;
+        pthread_cond_signal(&cond);  
     }
+    pthread_mutex_unlock(&lock);
 }
 
-void *respond_with_string(void *arg)
-{
-    char buffer[BUFFER_SIZE];
-    while (1)
-    {
-        char *string_response = "Response String";
-        write(pipe_str[1], string_response, strlen(string_response) + 1);
-
-        pthread_mutex_lock(&lock);
-        request_counter++;
-        printf("String enviada: %s (Contagem de requisições: %d)\n", string_response, request_counter);
-        pthread_mutex_unlock(&lock);
-        sleep(1);
+Task get_task() {
+    Task task;
+    pthread_mutex_lock(&lock);
+    while (task_count == 0) {  
+        pthread_cond_wait(&cond, &lock);
     }
+    task = task_queue[0];  
+    for (int i = 0; i < task_count - 1; i++) {
+        task_queue[i] = task_queue[i + 1];  
+    }
+    task_count--;
+    pthread_mutex_unlock(&lock);
+    return task;
 }
 
-void server()
-{
-    pipe(pipe_num);
-    pipe(pipe_str);
+void respond_with_number(void* arg) {
+    char buffer[BUFFER_SIZE];
+    int number = rand() % 100 + 1;  
+    snprintf(buffer, BUFFER_SIZE, "%d", number);
+
+    int fd = open(PIPE_NUM, O_WRONLY);
+    write(fd, buffer, strlen(buffer) + 1);  
+    close(fd);
+
+    pthread_mutex_lock(&lock);
+    request_counter++;
+    printf("Número enviado: %d (Contagem de requisições: %d)\n", number, request_counter);
+    pthread_mutex_unlock(&lock);
+}
+
+void respond_with_string(void* arg) {
+    char* string_response = "Response String";
+    
+    int fd = open(PIPE_STR, O_WRONLY);
+    write(fd, string_response, strlen(string_response) + 1);  
+    close(fd);
+
+    pthread_mutex_lock(&lock);
+    request_counter++;
+    printf("String enviada: %s (Contagem de requisições: %d)\n", string_response, request_counter);
+    pthread_mutex_unlock(&lock);
+}
+
+void* thread_function(void* arg) {
+    while (1) {
+        Task task = get_task();  
+        task.function(task.arg); 
+    }
+    return NULL;
+}
+
+void server() {
+    mkfifo(PIPE_NUM, 0666); 
+    mkfifo(PIPE_STR, 0666);
+
+    sleep(1);
 
     pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&cond, NULL);
 
-    pthread_t num_threads[2];
-    for (int i = 0; i < 2; i++)
-    {
-        pthread_create(&num_threads[i], NULL, respond_with_number, NULL);
+    pthread_t thread_pool[THREAD_POOL_SIZE];
+    for (int i = 0; i < THREAD_POOL_SIZE; i++) {
+        pthread_create(&thread_pool[i], NULL, thread_function, NULL);
     }
 
-    pthread_t str_threads[2];
-    for (int i = 0; i < 2; i++)
-    {
-        pthread_create(&str_threads[i], NULL, respond_with_string, NULL);
+    while (1) {
+        add_task(respond_with_number, NULL);  
+        add_task(respond_with_string, NULL);  
+        sleep(1); 
     }
 
-    char buffer[BUFFER_SIZE];
-    while (1)
-    {
-        if (read(pipe_num[0], buffer, BUFFER_SIZE) > 0)
-        {
-            printf("Cliente Número: %s\n", buffer);
-        }
-        if (read(pipe_str[0], buffer, BUFFER_SIZE) > 0)
-        {
-            printf("Cliente String: %s\n", buffer);
-        }
+    for (int i = 0; i < THREAD_POOL_SIZE; i++) {
+        pthread_join(thread_pool[i], NULL);
     }
 
     pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&cond);
+
+    unlink(PIPE_NUM);
+    unlink(PIPE_STR);
 }
 
-int main()
-{
-    srand(time(NULL));
-    server();
+
+int main() {
+    srand(time(NULL));  
+    server();         
     return 0;
 }
