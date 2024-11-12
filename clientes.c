5594faae1c5481fcc3aa768d45d4b1af5358896c
Guilherme Reis <guiglreis@gmail.com>
2024-11-12 00:40:59 -0300

    .

--- clientes.c
+++ clientes.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> //POSIX
+#include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 
@@ -8,36 +8,37 @@
 #define PIPE_NUM "/tmp/pipe_num"
 #define PIPE_STR "/tmp/pipe_str"
 
-//ler o numero recebido do servidor
-void receive_number()
-{
+int main() {
+    // Abrir os pipes uma vez no início em modo não bloqueante
+    int num_pipe = open(PIPE_NUM, O_RDONLY | O_NONBLOCK);
+    int str_pipe = open(PIPE_STR, O_RDONLY | O_NONBLOCK);
+
+    // Verificar se os pipes foram abertos com sucesso
+    if (num_pipe == -1 || str_pipe == -1) {
+        perror("Falha ao abrir os pipes");
+        return 1;
+    }
+
     char buffer[BUFFER_SIZE];
-    int opened_pipe = open(PIPE_NUM, O_RDONLY);
-    read(opened_pipe, buffer, BUFFER_SIZE);
-    close(opened_pipe);
+    while (1) {
+        // Ler número do PIPE_NUM
+        int num_read = read(num_pipe, buffer, BUFFER_SIZE);
+        if (num_read > 0) {
+            printf("Número recebido: %s\n", buffer);
+        }
 
-    printf("Número recebido: %s\n", buffer);
-}
+        // Ler trecho da música do PIPE_STR
+        int str_read = read(str_pipe, buffer, BUFFER_SIZE);
+        if (str_read > 0) {
+            printf("Trecho recebido: %s\n", buffer);
+        }
 
-//ler a string recebida do servidor
-void receive_string()
-{
-    char buffer[BUFFER_SIZE];
-    int opened_pipe = open(PIPE_STR, O_RDONLY);
-    read(opened_pipe, buffer, BUFFER_SIZE);
-    close(opened_pipe);
-
-    printf("Trecho recebida: %s\n", buffer);
-}
-
-int main()
-{
-    //bem bolado pra fazer a alternancia entre o num e a str e escrever no terminal RUNTIME
-    while (1)
-    {
-        receive_number();
-        receive_string();
+        // Pausa de 1 segundo antes de ler novamente
         sleep(1);
     }
+
+    // Fechar os pipes quando o programa terminar
+    close(num_pipe);
+    close(str_pipe);
     return 0;
 }
--- servidor.c
+++ servidor.c
@@ -9,57 +9,47 @@
 #include <cJSON.h>
 
 #define BUFFER_SIZE 256
-#define PIPE_NUM "/tmp/pipe_num" // FIFO para números
-#define PIPE_STR "/tmp/pipe_str" // FIFO para strings
+#define PIPE_NUM "/tmp/pipe_num"
+#define PIPE_STR "/tmp/pipe_str"
+#define NUM_THREADS 20
 
-// Estrutura para acumular dados da requisição
 struct data_build {
     char *ptr_char;
     size_t len_data;
 };
 
-// Inicializa a estrutura e aloca memória
 void init_struct(struct data_build *access_struct) {
     access_struct->len_data = 0;
     access_struct->ptr_char = malloc(access_struct->len_data + 1);
-  
     access_struct->ptr_char[0] = '\0';
 }
 
-// Callback para acumular os dados do HTTP
 size_t my_callback_data(void *ptr_char, size_t size, size_t num_received, struct data_build *access_struct) {
     size_t new_len = access_struct->len_data + size * num_received;
     access_struct->ptr_char = realloc(access_struct->ptr_char, new_len + 1);
-   
     memcpy(access_struct->ptr_char + access_struct->len_data, ptr_char, size * num_received);
     access_struct->ptr_char[new_len] = '\0';
     access_struct->len_data = new_len;
-
     return size * num_received;
 }
 
-// Obtém e seleciona um trecho da música
 void get_song_extract_and_select(char *selected_song_excerpt) {
-    CURL *curl_handle; //manipular o dado
-    CURLcode res; //retorno da library
-
-    struct data_build response; //acumulo
-    init_struct(&response); //malloc
-
+    CURL *curl_handle;
+    CURLcode res;
+    struct data_build response;
+    init_struct(&response);
     char api_url[256];
-    snprintf(api_url, sizeof(api_url), "https://api.vagalume.com.br/search.php?art=Zezé%%20di%%20Camargo%%20e%%20Luciano&mus=Flores%%20em%%20Vida&apikey=88f28adee8598d94334ceb6f0b32a9fd"); //formatando e armazenando a url da api
+    snprintf(api_url, sizeof(api_url), "https://api.vagalume.com.br/search.php?art=Zezé%%20di%%20Camargo%%20e%%20Luciano&mus=Flores%%20em%%20Vida&apikey=4e7ca5ee6e52499353ba00d3118a6610");
 
     curl_handle = curl_easy_init();
     if (curl_handle) {
         curl_easy_setopt(curl_handle, CURLOPT_URL, api_url);
         curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, my_callback_data);
         curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &response);
-
         res = curl_easy_perform(curl_handle);
         if (res != CURLE_OK) {
             fprintf(stderr, "O curl_easy_perform() falhou: %s\n", curl_easy_strerror(res));
         }
-
         cJSON *json = cJSON_Parse(response.ptr_char);
         if (json) {
             cJSON *mus = cJSON_GetObjectItemCaseSensitive(json, "mus");
@@ -88,38 +78,39 @@ void get_song_extract_and_select(char *selected_song_excerpt) {
             }
             cJSON_Delete(json);
         }
-
         free(response.ptr_char);
         curl_easy_cleanup(curl_handle);
     }
 }
 
-// Envia um número aleatório para o pipe
 void send_random_num() {
     char buffer[BUFFER_SIZE];
     int num = rand() % 100 + 1;
     snprintf(buffer, BUFFER_SIZE, "%d", num);
-
     int pipe_fd = open(PIPE_NUM, O_WRONLY);
-
-    write(pipe_fd, buffer, strlen(buffer) + 1);
-    close(pipe_fd);
-    printf("Número enviado: %d\n", num);
+    if (pipe_fd != -1) {
+        write(pipe_fd, buffer, strlen(buffer) + 1);
+        close(pipe_fd);
+        printf("Número enviado: %d\n", num);
+    } else {
+        perror("Falha ao abrir o PIPE_NUM para escrita");
+    }
 }
 
-// Envia um trecho da música para o pipe
 void send_song_excerpt() {
     char song_buffer[BUFFER_SIZE];
     get_song_extract_and_select(song_buffer);
-
     int pipe_fd = open(PIPE_STR, O_WRONLY);
-    
-    write(pipe_fd, song_buffer, strlen(song_buffer) + 1);
-    close(pipe_fd);
-    printf("Trecho enviado: %s\n", song_buffer);
+    if (pipe_fd != -1) {
+        write(pipe_fd, song_buffer, strlen(song_buffer) + 1);
+        close(pipe_fd);
+        printf("Trecho enviado: %s\n", song_buffer);
+    } else {
+        perror("Falha ao abrir o PIPE_STR para escrita");
+    }
 }
 
-void *thread_function() {
+void *thread_function(void *arg) {
     while (1) {
         send_random_num();
         send_song_excerpt();
@@ -129,16 +120,16 @@ void *thread_function() {
 }
 
 int main() {
-    mkfifo(PIPE_NUM, 0666);
-    mkfifo(PIPE_STR, 0666);
+    // Inicializa o pool de threads
+    pthread_t thread_pool[NUM_THREADS];
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&thread_pool[i], NULL, thread_function, NULL);
+    }
 
-    pthread_t thread_pool[1];
-    pthread_create(&thread_pool[0], NULL, thread_function, NULL);
-
-    pthread_join(thread_pool[0], NULL);
-
-    unlink(PIPE_NUM);
-    unlink(PIPE_STR);
+    // Aguarda as threads terminarem (não ocorrerá devido ao loop infinito)
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(thread_pool[i], NULL);
+    }
 
     return 0;
 }
